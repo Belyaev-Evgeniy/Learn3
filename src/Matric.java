a05f6abe665d2820e9ade082b2c5c3206c58ef80
Belyaev-Evgeniy <74003995+Belyaev-Evgeniy@users.noreply.github.com>
2022-06-23 18:38:53 +0400

    Программа перемножающая 2 матрицы

---
+++ src/Matric.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class Matric {
+
+    public static void main(String[] args) {
+
+        Calculate Quant = new Calculate();
+        Quant.Enter();
+    }
+}
